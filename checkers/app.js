ba01c24deee96bf9a058f5d391a9b4883d06902d
Matthew Hui <matt10145h@gmail.com>
2020-02-20 10:49:04 -0500

    remove images, add piece logic

--- checkers/app.js
+++ checkers/app.js
@@ -29,15 +29,37 @@ function init() {
             "", "", "", "", "", "", "", "",
             "", "", "", "", "", "", "", "",];
     turn = "BLACK";
+    for(i = 0; i < 25; i++) {
+        if (Math.floor(i/8) % 2 == 0 && i < 24) {
+            if (i % 2 != 0) {
+                
+            }
+        }
+        else if (Math.floor(i/8) % 2 != 0 && i < 24) {
+            if (i % 2 == 0) {
+
+            }
+        }
+    }
+    for(i = 62; i > 39; i--) {
+        if (Math.floor(i/8) % 2 == 0 && i > 37) {
+            if (i % 2 != 0) {
+
+            }
+        }
+        else if (Math.floor(i/8) % 2 != 0 && i > 37) {
+            if (i % 2 == 0) {
+
+            }
+        }
+    }
 
     render();
 }
 
 function render() {
-    for (let i = 0; i <= 64; i++) {
+    for ()
 
 
-
-    }
     turnUpdate.textContent = `TURN: ${turn}`;
 }
