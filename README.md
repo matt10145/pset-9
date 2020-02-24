f48aafdc76f386ba9a8c347fa817534fc2d3bceb
matth101 <mhui@ucvts.tec.nj.us>
2020-02-23 23:06:45 -0500

    Add brick breaker structuring

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Game Suite - Problem Set 9
 
 - Matthew Hui
+
+Checkers
+- add border radius / appending div logic
+- dynamically ids to each PIECE div
+
+https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Create_the_Canvas_and_draw_on_it
--- checkers/app.js
+++ checkers/app.js
@@ -30,35 +30,24 @@ function init() {
             "", "", "", "", "", "", "", "",];
     turn = "BLACK";
     for(i = 0; i < 25; i++) {
-        if (Math.floor(i/8) % 2 == 0 && i < 24) {
+        if ((Math.floor(i/8) % 2 == 0) && i < 24) {
             if (i % 2 != 0) {
                 
             }
         }
-        else if (Math.floor(i/8) % 2 != 0 && i < 24) {
+        else if ((Math.floor(i/8) % 2 != 0) && i < 24) {
             if (i % 2 == 0) {
 
             }
         }
     }
-    for(i = 62; i > 39; i--) {
-        if (Math.floor(i/8) % 2 == 0 && i > 37) {
-            if (i % 2 != 0) {
 
-            }
-        }
-        else if (Math.floor(i/8) % 2 != 0 && i > 37) {
-            if (i % 2 == 0) {
-
-            }
-        }
-    }
 
     render();
 }
 
 function render() {
-    for ()
+    
 
 
     turnUpdate.textContent = `TURN: ${turn}`;
