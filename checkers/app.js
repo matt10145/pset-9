9b3ffd010e0ecd6ab521caeb12fcfbfab197e6e2
matth101 <mhui@ucvts.tec.nj.us>
2020-03-01 23:50:27 -0500

    Comment in plan for checkers

--- checkers/app.js
+++ checkers/app.js
@@ -53,8 +53,7 @@ function render() {
 }
 
 /**
- * The actual function that runs on a checker piece click. Dynamically add event listeners
- * to the AVAILABLE spots for moving. Once a second part is clicked,
+ * The actual function that runs on a checker piece click. 
  */
 function takeTurn(e) {
     let target = e.target;
@@ -64,7 +63,7 @@ function takeTurn(e) {
         if (board[id] === "B") {
             if (areValidMoves(id, "black") !== []) {
                 moves = areValidMoves(id, "black");
-                
+
 
                 for (var i = 0; i <= moves.length; i++) {
                     moveTo = moves[i];
@@ -75,6 +74,13 @@ function takeTurn(e) {
             } 
         }
     }
+
+    // bruh literally just add another eventlistener to the whole board, use findindex to get
+    // the second clicked, and then match it up with the areValidMoves. If no matches, then 
+    // do nothing ? well actually use logic and check is areValidMoves is empty
+    // if it isn't then check
+    // if there are no matches, then... yeah do nothing
+
 
     // block turns by checking background color style of the piece
     //
@@ -169,7 +175,7 @@ function removePiece(index) {
  */
 function getWinner() {
     let amounts = [0, 0]; // first index black, second white
-    let winner;
+    let winner = "";
     let turns = 0;
 
     board.forEach((piece) => {
@@ -181,10 +187,10 @@ function getWinner() {
         }
     });
 
-    (amounts[0] === 0) ? winner = "White" : winner;
-    (amounts[1] === 0) ? winner = "Black" : winner;
+    (amounts[0] === 0) ? winner = "White" : winner = "";
+    (amounts[1] === 0) ? winner = "Black" : winner = "";
     (amounts[0] === 1 && amounts[1] === 1) ? turns++ : turns;
-    (turns >= 20)? winner = "Tie" : winner;
+    (turns >= 20)? winner = "Tie" : winner = ""; // stalemate safety
 
     return winner;
 }
