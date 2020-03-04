d3e86fd56c68841f4941366e15bebd272c6ef79b
matth101 <mhui@ucvts.tec.nj.us>
2020-03-04 02:40:09 -0500

    WROK

--- checkers/app.js
+++ checkers/app.js
@@ -7,7 +7,6 @@ let win;
 let whiteScore = 0;
 let blackScore = 0;
 let pieceSelected = false;
-let possibleMoves;
 let selectedPiece;
 let turns = 0;
 
@@ -24,6 +23,21 @@ document.getElementById("reset").onclick = init;
 selectOrTurn();
 
 ///// FUNCTIONS  /////
+
+/**
+ * Creates and appends the checker pieces.
+ * @param index the spot on the board that should be added
+ * @param color the color of the piece
+ */
+function createPiece(index, color) {
+    div = document.createElement("div");
+    outer = document.getElementById(squares[index].id);
+    div.style.height = "50px";
+    div.style.width = "50px";
+    div.style.borderRadius = "50%";
+    div.style.backgroundColor = color;
+    outer.append(div);
+}
 
 /**
  * Function that runs on startup.
@@ -50,32 +64,18 @@ function render() {
     board.forEach(function(mark, index) {
         squares[index].textContent = mark;
         squares[index].classList.remove('remove-9', 'remove-7', 'remove+9',
-        'remove+7', 'available-move', 'available-jump');
+        'remove+7', 'can-move', 'can-jump');
         (!squares[index].classList.contains('king') && mark == "W" && index > 55)?
           squares[index].classList.add('king') : index;
         (!squares[index].classList.contains('king') && mark == "B" && index < 8)?
           squares[index].classList.add('king') : index;
       });
-      turnUpdate.textContent =
-        win === "tie" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn.toUpperCase()}`;
-    
-    // board.forEach(function(piece, index) {
-    //     squares[index].textContent = piece;
-    //     squares[index].classList.remove('remove-9', 'remove-7', 'remove+9',
-    //     'remove+7', 'can-move', 'can-jump');
-    //     (!squares[index].classList.contains('king') && piece == "B" && index > 55) ?
-    //     squares[index].classList.add('king') : index;
-    //     (!squares[index].classList.contains('king') && piece == "W" && index < 8) ?
-    //     squares[index].classList.add('king') : index;
-    // });
 
-    // bScore.textContent = blackScore;
-    // wScore.textContent = whiteScore;
-    // turnUpdate.textContent = win === "tie" ? "TIE GAME" : win ? `${win} WINS!` : `TURN: ${turn}`;
+    turnUpdate.textContent = win === "tie" ? "TIE GAME" : win ? `${win} WINS` : `TURN: ${turn}`;
 }
 
 /**
- * Function bridging the logic between selecting amd moving pieces with selectPiece() and takeTurn().
+ * Function bridging the logic between selecting and moving pieces with selectPiece() and takeTurn().
  */
 function selectOrTurn() {
     if (pieceSelected) {
@@ -111,17 +111,20 @@ function takeTurn(e) {
     let index = squares.findIndex(function(square) {
         return square === e.target;
     });
-    (turn === "WHITE") ? getValidMoves(index, "W") : getValidMoves(index, "B");
+    (turn === "BLACK") ? getValidMovesB(index) : getValidMovesW(index);
 
-    if (board[index] === "" && index !== selectedPiece && squares[index].classList.contains("can-move")) {
-        (turn === "WHITE") ? board[index] = "W" : board[index] = "B";
+    console.log(squares[index].classList)
+
+    if (board[index] === "" && index !== selectedPiece &&
+    squares[index].classList.contains('can-move')) {
+        (turn === "BLACK") ? board[index] = "B" : board[index] = "W";
         if (squares[selectedPiece].classList.contains('king')) {
             squares[index].classList.add('king');
             squares[selectedPiece].classList.remove('king');
         }
         board[selectedPiece] = "";
         squares[selectedPiece].classList.toggle('selected-piece');
-        turn = turn === "WHITE" ? "BLACK" : "WHITE";
+        turn = turn === "BLACK" ? "WHITE" : "BLACK";
         win = getWinner();
         pieceSelected = false;
         render();
@@ -129,18 +132,17 @@ function takeTurn(e) {
         squares[selectedPiece].classList.toggle('selected-piece');
         pieceSelected = false;
         render();
-    } else if (board[index] === "" && index !== selectedPiece && squares[index].classList.contains("can-jump")) { 
+    } else if (board[index] === "" && index !== selectedPiece &&
+    squares[index].classList.contains("can-jump")) {
         if (squares[selectedPiece].classList.contains('king')) {
             squares[index].classList.add('king');
             squares[selectedPiece].classList.remove('king');
         }
-
-        (turn === "WHITE")? board[index] = "W" : board[index] = "B";
+        (turn === "BLACK") ? board[index] = "B": board[index] = "W";
         board[selectedPiece] = "";
         squares[selectedPiece].classList.toggle('selected-piece');
-        turn = turn === "WHITE" ? "BLACK" : "WHITE";
-
-        if (squares[index].classList.contains("remove-9")){
+        turn = turn === "BLACK" ? "WHITE" : "BLACK";
+        if (squares[index].classList.contains("remove-9")) {
             board[selectedPiece - 9] = "";
         } else if (squares[index].classList.contains("remove+9")) {
             board[selectedPiece + 9] = "";
@@ -159,104 +161,75 @@ function takeTurn(e) {
 /**
  * Add valid moves to the class list.
  * @param index index of the clicked piece
- * @param color color determining which side is being checked
  */
-function getValidMoves(index, color) {
-    if (color === "B") {
-        if (selectedPiece !== "" && board[index] == "") {
-            (selectedPiece % 8 !== 0 && board[selectedPiece + 7] === "") ?
-            squares[selectedPiece + 7].classList.add('can-move') : selectedPiece;
-            (selectedPiece % 8 !== 7 && board[selectedPiece + 9] === "") ?
-            squares[selectedPiece + 9].classList.add('can-move') : selectedPiece;
-            (selectedPiece % 8 !== 0 && selectedPiece % 8 !== 1 && board[selectedPiece + 14] === "" && board[selectedPiece+7] === "W")?
-            squares[selectedPiece + 14].classList.add('can-jump', 'remove+7') : selectedPiece;
-            (selectedPiece % 8 !== 7 && selectedPiece % 8 !== 6 && board[selectedPiece + 18] === "" && board[selectedPiece+9] === "W")?
-            squares[selectedPiece + 18].classList.add('can-jump', 'remove+9') : selectedPiece;
-        if (squares[selectedPiece].classList.contains('king')) {
-            (selectedPiece % 8 !== 7 && board[selectedPiece - 7] === "") ?
-            squares[selectedPiece - 7].classList.add('can-move') : selectedPiece;
-            (selectedPiece % 8 !== 0 && board[selectedPiece - 9] === "") ?
-            squares[selectedPiece - 9].classList.add('can-move') : selectedPiece;
-            (selectedPiece % 8 !== 1 && selectedPiece % 8 !== 0 && board[selectedPiece - 18] === "" && board[selectedPiece - 9] === "W")?
-            squares[selectedPiece - 18].classList.add('can-jump', 'remove-9') : selectedPiece;
-            (selectedPiece % 8 !== 6 && selectedPiece % 8 !== 7 && board[selectedPiece - 14] === "" && board[selectedPiece - 7] === "W")?
-            squares[selectedPiece - 14].classList.add('can-jump', 'remove-7') : selectedPiece;
-            }
+function getValidMovesW(index) {
+    if (selectedPiece !== "" && board[index] == "") {
+        (selectedPiece % 8 !== 0 && board[selectedPiece + 7] === "") ?
+        squares[selectedPiece + 7].classList.add('can-move') : selectedPiece;
+        (selectedPiece % 8 !== 7 && board[selectedPiece + 9] === "") ?
+        squares[selectedPiece + 9].classList.add('can-move') : selectedPiece;
+        (selectedPiece % 8 !== 0 && selectedPiece % 8 !== 1 && board[selectedPiece + 14] === "" && board[selectedPiece+7] === "B") ?
+        squares[selectedPiece + 14].classList.add('can-jump', 'remove+7') : selectedPiece;
+        (selectedPiece % 8 !== 7 && selectedPiece % 8 !== 6 && board[selectedPiece + 18] === "" && board[selectedPiece+9] === "B") ?
+        squares[selectedPiece + 18].classList.add('can-jump', 'remove+9') : selectedPiece;
+    if (squares[selectedPiece].classList.contains('king')) {
+        (selectedPiece % 8 !== 7 && board[selectedPiece - 7] === "") ?
+        squares[selectedPiece - 7].classList.add('can-move') : selectedPiece;
+        (selectedPiece % 8 !== 0 && board[selectedPiece - 9] === "") ?
+        squares[selectedPiece - 9].classList.add('can-move') : selectedPiece;
+        (selectedPiece % 8 !== 1 && selectedPiece % 8 !== 0 && board[selectedPiece - 18] === "" && board[selectedPiece - 9] === "B") ?
+        squares[selectedPiece - 18].classList.add('can-jump', 'remove-9') : selectedPiece;
+        (selectedPiece % 8 !== 6 && selectedPiece % 8 !== 7 && board[selectedPiece - 14] === "" && board[selectedPiece - 7] === "B") ?
+        squares[selectedPiece - 14].classList.add('can-jump', 'remove-7') : selectedPiece;
         }
-    }
-    if (color === "W") {
-        if (selectedPiece !== "" && board[index] === "") {
-            (selectedPiece % 8 !== 7 && board[selectedPiece - 7] === "") ?
-            squares[selectedPiece - 7].classList.add('can-move') : selectedPiece;
-            (selectedPiece % 8 !== 0 && board[selectedPiece - 9] === "") ?
-            squares[selectedPiece - 9].classList.add('can-move') : selectedPiece;
-            (selectedPiece % 8 !== 1 && selectedPiece % 8 !== 0 && board[selectedPiece - 18] === "" && board[selectedPiece - 9] === "B")?
-            squares[selectedPiece - 18].classList.add('can-jump', 'remove-9') : selectedPiece;
-            (selectedPiece % 8 !== 6 && selectedPiece % 8 !== 7 && board[selectedPiece - 14] === "" && board[selectedPiece - 7] === "B")?
-            squares[selectedPiece - 14].classList.add('can-jump', 'remove-7') : selectedPiece;
-        }
-        if (squares[selectedPiece].classList.contains('king')) {
-            (selectedPiece % 8 !==0 && board[selectedPiece + 7] === "") ?
-            squares[selectedPiece + 7].classList.add('can-move') : selectedPiece;
-            (selectedPiece % 8 !==7 && board[selectedPiece + 9] === "") ?
-            squares[selectedPiece + 9].classList.add('can-move') : selectedPiece;
-            (selectedPiece % 8 !==1 && selectedPiece % 8 !== 0 && board[selectedPiece + 14] === "" && board[selectedPiece + 7] === "B")?
-            squares[selectedPiece + 14].classList.add('can-jump', 'remove+7') : selectedPiece;
-            (selectedPiece % 8 !== 6 && selectedPiece % 8 !== 7 && board[selectedPiece + 18] === "" && board[selectedPiece + 9] === "B")?
-            squares[selectedPiece+18].classList.add('can-jump', 'remove+9') : selectedPiece;
-        }
-    
     }
 }
 
-/**
- * Creates and appends the checker pieces.
- * @param index the spot on the board that should be added
- * @param color the color of the piece
- */
-function createPiece(index, color) {
-    div = document.createElement("div");
-    outer = document.getElementById(squares[index].id);
-    div.style.height = "50px";
-    div.style.width = "50px";
-    div.style.borderRadius = "50%";
-    div.style.backgroundColor = color;
-    outer.append(div);
+function getValidMovesB(index) {
+    if (selectedPiece !== "" && board[index] === "") {
+        (selectedPiece % 8 !== 7 && board[selectedPiece - 7] === "") ?
+        squares[selectedPiece - 7].classList.add('can-move') : selectedPiece;
+        (selectedPiece % 8 !== 0 && board[selectedPiece - 9] === "") ?
+        squares[selectedPiece - 9].classList.add('can-move') : selectedPiece;
+        (selectedPiece % 8 !== 1 && selectedPiece % 8 !== 0 && board[selectedPiece - 18] === "" && board[selectedPiece - 9] === "W") ?
+        squares[selectedPiece - 18].classList.add('can-jump', 'remove-9') : selectedPiece;
+        (selectedPiece % 8 !== 6 && selectedPiece % 8 !== 7 && board[selectedPiece - 14] === "" && board[selectedPiece - 7] === "W") ?
+        squares[selectedPiece - 14].classList.add('can-jump', 'remove-7') : selectedPiece;
+    }
+    if (squares[selectedPiece].classList.contains('king')) {
+        (selectedPiece % 8 !== 0 && board[selectedPiece + 7] === "") ?
+        squares[selectedPiece + 7].classList.add('can-move') : selectedPiece;
+        (selectedPiece % 8 !== 7 && board[selectedPiece + 9] === "") ?
+        squares[selectedPiece + 9].classList.add('can-move') : selectedPiece;
+        (selectedPiece % 8 !== 1 && selectedPiece % 8 !== 0 && board[selectedPiece + 14] === "" && board[selectedPiece + 7] === "W")?
+        squares[selectedPiece + 14].classList.add('can-jump', 'remove+7') : selectedPiece;
+        (selectedPiece % 8 !== 6 && selectedPiece % 8 !== 7 && board[selectedPiece + 18] === "" && board[selectedPiece + 9] === "W")?
+        squares[selectedPiece + 18].classList.add('can-jump', 'remove+9') : selectedPiece;
+    }
 }
+
 
 /**
  * Uses the present number of pieces to check if the game has been won or tied.
  */
 function getWinner() {
+    let white = 0;
+    let black = 0;
     let winner = null;
-    let countBlack = 0;
-    let countWhite = 0;
-    board.forEach(function(mark){
-      (mark === "B")? countBlack++: countBlack;
-      (mark === "W")? countWhite++: countWhite;
+
+    board.forEach((piece) => {
+        if (piece === "B") {
+            black++;
+        }
+        if (piece === "W") {
+            white++;
+        }
     });
-    (countBlack === 0)? winner = "White": winner;
-    (countWhite === 0)? winner = "Black": winner;
-    (countBlack === 1 && countWhite === 1)? turnCount++ : turnCount;
-    (turnCount>=20)? winner = "tie" : winner;
-    return winner;  
-    // let amounts = [0, 0]; // first index black, second white
-    // let winner = "";
-    // let turns = 0;
 
-    // board.forEach((piece) => {
-    //     if (piece === "B") {
-    //         amounts[0] += 1;
-    //     }
-    //     if (piece === "W") {
-    //         amounts[1] += 1;
-    //     }
-    // });
+    (black === 0) ? winner = "WHITE" : winner = "";
+    (white === 0) ? winner = "BLACK" : winner = "";
+    (black === 1 && white === 1) ? turns++ : turns;
+    (turns >= 20)? winner = "tie" : winner = ""; // stalemate safety
 
-    // (amounts[0] === 0) ? winner = "White" : winner = "";
-    // (amounts[1] === 0) ? winner = "Black" : winner = "";
-    // (amounts[0] === 1 && amounts[1] === 1) ? turns++ : turns;
-    // (turns >= 20)? winner = "tie" : winner = ""; // stalemate safety
-
-    // return winner;
+    return winner;
 }
